02900a2de501e621b6c25bbc53317898e75d884c
ChazzitUP <hamzarezgui.education@gmail.com>
2022-04-23 16:39:27 +0100

    implemented intro component, styles

--- src/components/intro/Intro.component.jsx
+++ src/components/intro/Intro.component.jsx
@@ -4,6 +4,22 @@ import './Intro.styles.scss'
 
 export default function Intro() {
   return (
-    <div className='intro' id='intro'></div>
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src='assets/gheryel.png' alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Hamza Rezgui</h1>
+          <h3>Freelance <span></span></h3>
+        </div>
+        <a href="#portefolio">
+          <img src="assets/downarrow.png" alt="" />
+        </a>
+      </div>
+    </div>
   )
 }
